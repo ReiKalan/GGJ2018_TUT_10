efb96d73109a8abaab8306372fbc007f8fffaa1d
nakamurayoyoyo <ynakamura@edu.teu.ac.jp>
2018-01-27 17:15:28 +0900

    UI追加と整理

---
+++ Assets/UI/scripts/hasami.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hasami : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        int rand = Random.Range(0, 99);
+        Debug.Log(rand);
+        switch (rand)
+        {
+            case 0:
+                SceneManager.LoadScene("clear3");
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                SceneManager.LoadScene("clear2");
+                break;
+            default:
+                SceneManager.LoadScene("clear1");
+                break;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/UI/scripts/koa.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class koa : MonoBehaviour {
+
+    [SerializeField] Graphic gra;
+    [SerializeField] float ing=1.0f;
+    [SerializeField] float dei = 0.333f;
+    Coroutine cor;
+    
+  
+    private Text text;
+
+    private float nextTime;
+    public float interval;
+
+     void Reset()
+    {
+        gra = GetComponent<Graphic>();
+    }
+     void Awake()
+    {
+        StartFlash();   
+    }
+
+
+    // Use this for initialization
+    IEnumerator Flash() {
+        float time = 0.0f;
+        text = GetComponent<Text>();
+
+        while (true)
+        {
+            time += ing * dei;
+
+            var color = gra.color;
+            color.a = Mathf.Abs(Mathf.Sin(time));
+            gra.color = color;
+            yield return new WaitForSeconds(dei);
+        }
+
+        
+
+        
+	}
+
+    public void StartFlash()
+    {
+        cor = StartCoroutine(Flash());
+    }
+
+    public void StopFlash()
+    {
+        StopCoroutine(cor);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+       if(Time.time>nextTime)
+        {
+           
+        }
+    }
+}
